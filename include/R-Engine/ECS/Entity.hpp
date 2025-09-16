3d5801a065ba885cb1376ca2e1954248fd836679
Leorevoir <leo.quinzler@epitech.eu>
2025-09-16 11:08:28 +0200

    fix: ECS::Entity -> make it an u32

--- include/R-Engine/ECS/Entity.hpp
+++ include/R-Engine/ECS/Entity.hpp
@@ -1,35 +1,12 @@
 #pragma once
 
-#include <R-Engine/Core/BitMask.hpp>
-#include <R-Engine/ECS/Component.hpp>
 #include <R-Engine/Types.hpp>
-
-#include <memory>
-#include <vector>
 
 namespace r {
 
 namespace ecs {
 
-class Entity : public NonCopyable
-{
-    public:
-        explicit Entity(const u64 id, const bool active = true);
-
-        u64 getID() const;
-        bool isActive() const;
-
-        const std::vector<std::unique_ptr<Component>> &getComponents() const;
-        const core::BitMask &getEnabledComponents() const;
-
-        void setActive(const bool active);
-
-    private:
-        u64 _id;
-        bool _active;
-        std::vector<std::unique_ptr<Component>> _components;
-        core::BitMask _enabled_components;
-};
+using Entity = u32;
 
 }// namespace ecs
 

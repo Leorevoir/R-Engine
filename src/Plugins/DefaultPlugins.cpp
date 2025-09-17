e2736d9a5c1b37e57991c9c89737f587a290540e
Leorevoir <leo.quinzler@epitech.eu>
2025-09-17 09:35:15 +0200

    add: DefaultPlugins

---
+++ src/Plugins/DefaultPlugins.cpp
@@ -0,0 +1,13 @@
+#include <R-Engine/Plugins/DefaultPlugins.hpp>
+#include <R-Engine/Plugins/WindowPlugin.hpp>
+
+#include <R-Engine/Application.hpp>
+
+/**
+* public
+*/
+
+void r::DefaultPlugins::build(r::Application &app)
+{
+    app.add_plugins<r::WindowPlugin>();
+}

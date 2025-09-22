4684918dec9a2a869ec906da7e0fe67b6eed8cac
Leorevoir <leo.quinzler@epitech.eu>
2025-09-22 11:19:23 +0200

    feat: better file architecture -> scripts/Windows

--- scripts/Windows/R-Engine_Windows_Installer/Form1.Designer.cs
+++ scripts/Windows/R-Engine_Windows_Installer/Form1.Designer.cs
@@ -31,15 +31,15 @@
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblDescription = new System.Windows.Forms.Label();
             this.grpRequirements = new System.Windows.Forms.GroupBox();
-            this.lblCMakeStatus = new System.Windows.Forms.Label();
-            this.lblNinjaStatus = new System.Windows.Forms.Label();
-            this.lblCompilerStatus = new System.Windows.Forms.Label();
-            this.picCMake = new System.Windows.Forms.PictureBox();
-            this.picNinja = new System.Windows.Forms.PictureBox();
-            this.picCompiler = new System.Windows.Forms.PictureBox();
-            this.lblCMake = new System.Windows.Forms.Label();
-            this.lblNinja = new System.Windows.Forms.Label();
             this.lblCompiler = new System.Windows.Forms.Label();
+            this.lblNinja = new System.Windows.Forms.Label();
+            this.lblCMake = new System.Windows.Forms.Label();
+            this.picCompiler = new System.Windows.Forms.PictureBox();
+            this.picNinja = new System.Windows.Forms.PictureBox();
+            this.picCMake = new System.Windows.Forms.PictureBox();
+            this.lblCompilerStatus = new System.Windows.Forms.Label();
+            this.lblNinjaStatus = new System.Windows.Forms.Label();
+            this.lblCMakeStatus = new System.Windows.Forms.Label();
             this.btnInstallMissing = new System.Windows.Forms.Button();
             this.btnCheckAgain = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
@@ -47,27 +47,29 @@
             this.lblProgress = new System.Windows.Forms.Label();
             this.rtbLog = new System.Windows.Forms.RichTextBox();
             this.grpRequirements.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picCMake)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picNinja)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCompiler)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNinja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCMake)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Location = new System.Drawing.Point(24, 17);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(308, 26);
+            this.lblTitle.Size = new System.Drawing.Size(618, 51);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "R-Engine Development Setup";
             // 
             // lblDescription
             // 
             this.lblDescription.AutoSize = true;
-            this.lblDescription.Location = new System.Drawing.Point(14, 44);
+            this.lblDescription.Location = new System.Drawing.Point(28, 85);
+            this.lblDescription.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(503, 13);
+            this.lblDescription.Size = new System.Drawing.Size(1062, 25);
             this.lblDescription.TabIndex = 1;
             this.lblDescription.Text = "This installer will check and install the required development tools for R-Engine" +
     " (CMake, Ninja, C++ Compiler).";
@@ -83,100 +85,112 @@
             this.grpRequirements.Controls.Add(this.lblCompilerStatus);
             this.grpRequirements.Controls.Add(this.lblNinjaStatus);
             this.grpRequirements.Controls.Add(this.lblCMakeStatus);
-            this.grpRequirements.Location = new System.Drawing.Point(17, 70);
+            this.grpRequirements.Location = new System.Drawing.Point(34, 135);
+            this.grpRequirements.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.grpRequirements.Name = "grpRequirements";
-            this.grpRequirements.Size = new System.Drawing.Size(500, 120);
+            this.grpRequirements.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.grpRequirements.Size = new System.Drawing.Size(1000, 231);
             this.grpRequirements.TabIndex = 2;
             this.grpRequirements.TabStop = false;
             this.grpRequirements.Text = "Development Tools Status";
-            // 
-            // lblCMakeStatus
-            // 
-            this.lblCMakeStatus.AutoSize = true;
-            this.lblCMakeStatus.Location = new System.Drawing.Point(130, 25);
-            this.lblCMakeStatus.Name = "lblCMakeStatus";
-            this.lblCMakeStatus.Size = new System.Drawing.Size(62, 13);
-            this.lblCMakeStatus.TabIndex = 0;
-            this.lblCMakeStatus.Text = "Checking...";
-            // 
-            // lblNinjaStatus
-            // 
-            this.lblNinjaStatus.AutoSize = true;
-            this.lblNinjaStatus.Location = new System.Drawing.Point(130, 55);
-            this.lblNinjaStatus.Name = "lblNinjaStatus";
-            this.lblNinjaStatus.Size = new System.Drawing.Size(62, 13);
-            this.lblNinjaStatus.TabIndex = 1;
-            this.lblNinjaStatus.Text = "Checking...";
-            // 
-            // lblCompilerStatus
-            // 
-            this.lblCompilerStatus.AutoSize = true;
-            this.lblCompilerStatus.Location = new System.Drawing.Point(130, 85);
-            this.lblCompilerStatus.Name = "lblCompilerStatus";
-            this.lblCompilerStatus.Size = new System.Drawing.Size(62, 13);
-            this.lblCompilerStatus.TabIndex = 2;
-            this.lblCompilerStatus.Text = "Checking...";
-            // 
-            // picCMake
-            // 
-            this.picCMake.Location = new System.Drawing.Point(15, 20);
-            this.picCMake.Name = "picCMake";
-            this.picCMake.Size = new System.Drawing.Size(24, 24);
-            this.picCMake.TabIndex = 3;
-            this.picCMake.TabStop = false;
-            // 
-            // picNinja
-            // 
-            this.picNinja.Location = new System.Drawing.Point(15, 50);
-            this.picNinja.Name = "picNinja";
-            this.picNinja.Size = new System.Drawing.Size(24, 24);
-            this.picNinja.TabIndex = 4;
-            this.picNinja.TabStop = false;
-            // 
-            // picCompiler
-            // 
-            this.picCompiler.Location = new System.Drawing.Point(15, 80);
-            this.picCompiler.Name = "picCompiler";
-            this.picCompiler.Size = new System.Drawing.Size(24, 24);
-            this.picCompiler.TabIndex = 5;
-            this.picCompiler.TabStop = false;
-            // 
-            // lblCMake
-            // 
-            this.lblCMake.AutoSize = true;
-            this.lblCMake.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCMake.Location = new System.Drawing.Point(50, 25);
-            this.lblCMake.Name = "lblCMake";
-            this.lblCMake.Size = new System.Drawing.Size(47, 13);
-            this.lblCMake.TabIndex = 6;
-            this.lblCMake.Text = "CMake";
-            // 
-            // lblNinja
-            // 
-            this.lblNinja.AutoSize = true;
-            this.lblNinja.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNinja.Location = new System.Drawing.Point(50, 55);
-            this.lblNinja.Name = "lblNinja";
-            this.lblNinja.Size = new System.Drawing.Size(35, 13);
-            this.lblNinja.TabIndex = 7;
-            this.lblNinja.Text = "Ninja";
             // 
             // lblCompiler
             // 
             this.lblCompiler.AutoSize = true;
             this.lblCompiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCompiler.Location = new System.Drawing.Point(50, 85);
+            this.lblCompiler.Location = new System.Drawing.Point(100, 163);
+            this.lblCompiler.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCompiler.Name = "lblCompiler";
-            this.lblCompiler.Size = new System.Drawing.Size(79, 13);
+            this.lblCompiler.Size = new System.Drawing.Size(160, 26);
             this.lblCompiler.TabIndex = 8;
             this.lblCompiler.Text = "C++ Compiler";
+            // 
+            // lblNinja
+            // 
+            this.lblNinja.AutoSize = true;
+            this.lblNinja.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNinja.Location = new System.Drawing.Point(100, 106);
+            this.lblNinja.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblNinja.Name = "lblNinja";
+            this.lblNinja.Size = new System.Drawing.Size(67, 26);
+            this.lblNinja.TabIndex = 7;
+            this.lblNinja.Text = "Ninja";
+            // 
+            // lblCMake
+            // 
+            this.lblCMake.AutoSize = true;
+            this.lblCMake.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCMake.Location = new System.Drawing.Point(100, 48);
+            this.lblCMake.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCMake.Name = "lblCMake";
+            this.lblCMake.Size = new System.Drawing.Size(86, 26);
+            this.lblCMake.TabIndex = 6;
+            this.lblCMake.Text = "CMake";
+            // 
+            // picCompiler
+            // 
+            this.picCompiler.Location = new System.Drawing.Point(30, 154);
+            this.picCompiler.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.picCompiler.Name = "picCompiler";
+            this.picCompiler.Size = new System.Drawing.Size(48, 46);
+            this.picCompiler.TabIndex = 5;
+            this.picCompiler.TabStop = false;
+            // 
+            // picNinja
+            // 
+            this.picNinja.Location = new System.Drawing.Point(30, 96);
+            this.picNinja.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.picNinja.Name = "picNinja";
+            this.picNinja.Size = new System.Drawing.Size(48, 46);
+            this.picNinja.TabIndex = 4;
+            this.picNinja.TabStop = false;
+            // 
+            // picCMake
+            // 
+            this.picCMake.Location = new System.Drawing.Point(30, 38);
+            this.picCMake.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.picCMake.Name = "picCMake";
+            this.picCMake.Size = new System.Drawing.Size(48, 46);
+            this.picCMake.TabIndex = 3;
+            this.picCMake.TabStop = false;
+            // 
+            // lblCompilerStatus
+            // 
+            this.lblCompilerStatus.AutoSize = true;
+            this.lblCompilerStatus.Location = new System.Drawing.Point(260, 163);
+            this.lblCompilerStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCompilerStatus.Name = "lblCompilerStatus";
+            this.lblCompilerStatus.Size = new System.Drawing.Size(120, 25);
+            this.lblCompilerStatus.TabIndex = 2;
+            this.lblCompilerStatus.Text = "Checking...";
+            // 
+            // lblNinjaStatus
+            // 
+            this.lblNinjaStatus.AutoSize = true;
+            this.lblNinjaStatus.Location = new System.Drawing.Point(260, 106);
+            this.lblNinjaStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblNinjaStatus.Name = "lblNinjaStatus";
+            this.lblNinjaStatus.Size = new System.Drawing.Size(120, 25);
+            this.lblNinjaStatus.TabIndex = 1;
+            this.lblNinjaStatus.Text = "Checking...";
+            // 
+            // lblCMakeStatus
+            // 
+            this.lblCMakeStatus.AutoSize = true;
+            this.lblCMakeStatus.Location = new System.Drawing.Point(260, 48);
+            this.lblCMakeStatus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCMakeStatus.Name = "lblCMakeStatus";
+            this.lblCMakeStatus.Size = new System.Drawing.Size(120, 25);
+            this.lblCMakeStatus.TabIndex = 0;
+            this.lblCMakeStatus.Text = "Checking...";
             // 
             // btnInstallMissing
             // 
             this.btnInstallMissing.Enabled = false;
-            this.btnInstallMissing.Location = new System.Drawing.Point(17, 206);
+            this.btnInstallMissing.Location = new System.Drawing.Point(34, 396);
+            this.btnInstallMissing.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnInstallMissing.Name = "btnInstallMissing";
-            this.btnInstallMissing.Size = new System.Drawing.Size(120, 30);
+            this.btnInstallMissing.Size = new System.Drawing.Size(240, 58);
             this.btnInstallMissing.TabIndex = 3;
             this.btnInstallMissing.Text = "Install Missing Tools";
             this.btnInstallMissing.UseVisualStyleBackColor = true;
@@ -184,9 +198,10 @@
             // 
             // btnCheckAgain
             // 
-            this.btnCheckAgain.Location = new System.Drawing.Point(153, 206);
+            this.btnCheckAgain.Location = new System.Drawing.Point(306, 396);
+            this.btnCheckAgain.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCheckAgain.Name = "btnCheckAgain";
-            this.btnCheckAgain.Size = new System.Drawing.Size(100, 30);
+            this.btnCheckAgain.Size = new System.Drawing.Size(200, 58);
             this.btnCheckAgain.TabIndex = 4;
             this.btnCheckAgain.Text = "Check Again";
             this.btnCheckAgain.UseVisualStyleBackColor = true;
@@ -194,9 +209,10 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(442, 206);
+            this.btnClose.Location = new System.Drawing.Point(884, 396);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 30);
+            this.btnClose.Size = new System.Drawing.Size(150, 58);
             this.btnClose.TabIndex = 5;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -204,35 +220,38 @@
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(17, 252);
+            this.progressBar.Location = new System.Drawing.Point(34, 485);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(500, 20);
+            this.progressBar.Size = new System.Drawing.Size(1000, 38);
             this.progressBar.TabIndex = 6;
             this.progressBar.Visible = false;
             // 
             // lblProgress
             // 
             this.lblProgress.AutoSize = true;
-            this.lblProgress.Location = new System.Drawing.Point(17, 285);
+            this.lblProgress.Location = new System.Drawing.Point(34, 548);
+            this.lblProgress.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblProgress.Name = "lblProgress";
-            this.lblProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblProgress.Size = new System.Drawing.Size(0, 25);
             this.lblProgress.TabIndex = 7;
             // 
             // rtbLog
             // 
-            this.rtbLog.Location = new System.Drawing.Point(17, 308);
+            this.rtbLog.Location = new System.Drawing.Point(34, 592);
+            this.rtbLog.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rtbLog.Name = "rtbLog";
             this.rtbLog.ReadOnly = true;
-            this.rtbLog.Size = new System.Drawing.Size(500, 130);
+            this.rtbLog.Size = new System.Drawing.Size(996, 246);
             this.rtbLog.TabIndex = 8;
             this.rtbLog.Text = "";
             this.rtbLog.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 261);
+            this.ClientSize = new System.Drawing.Size(1068, 502);
             this.Controls.Add(this.rtbLog);
             this.Controls.Add(this.lblProgress);
             this.Controls.Add(this.progressBar);
@@ -243,6 +262,7 @@
             this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -251,11 +271,12 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.grpRequirements.ResumeLayout(false);
             this.grpRequirements.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picCMake)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picNinja)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCompiler)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picNinja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCMake)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion

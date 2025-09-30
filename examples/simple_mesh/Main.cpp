f9dc1cffcf0575a6b03cc2eeafc4da56b4a6dcce
Leorevoir <leo.quinzler@epitech.eu>
2025-09-30 17:55:42 +0200

    add: example main for simple mesh

--- examples/simple_mesh/Main.cpp
+++ examples/simple_mesh/Main.cpp
@@ -15,7 +15,7 @@ static void startup_system(r::ecs::Commands &cmd, r::ecs::ResMut<r::Meshes> mesh
             meshes.ptr->add(r::Mesh3d::Cube(1.f), texture_path),
             r::Color{ 0, 128, 0}
         },
-        r::Transform3d{.translation = {0,0,0}, .scale = {1,1,1}}
+        r::Transform3d{.translation = {0, 0, 0}, .scale = {1, 1, 1}}
     );
 }
 

ffa232d90ed76e14e549b31a70103ed80da7531b
Leorevoir <leo.quinzler@epitech.eu>
2025-09-30 18:57:44 +0200

    move: assets/ path

--- examples/simple_mesh/Main.cpp
+++ examples/simple_mesh/Main.cpp
@@ -2,16 +2,17 @@
 #include "R-Engine/Plugins/DefaultPlugins.hpp"
 #include "R-Engine/Plugins/MeshPlugin.hpp"
 #include "R-Engine/Plugins/WindowPlugin.hpp"
+#include <R-Engine/Core/Filepath.hpp>
 
 // clang-format off
 
 static void startup_system(r::ecs::Commands &cmd, r::ecs::ResMut<r::Meshes> meshes)
 {
-    // const std::string &texture_path = r::path::get("./assets/textures/rock.png");
+    const std::string &texture_path = r::path::get("./examples/assets/textures/rock.png");
 
     cmd.spawn(
         r::Mesh3d{
-            meshes.ptr->add(r::Mesh3d::Cube(1.f)),
+            meshes.ptr->add(r::Mesh3d::Cube(1.f), texture_path),
             r::Color{ 0, 128, 0}
         },
         r::Transform3d{.translation = {0, 0, 0}, .scale = {1, 1, 1}}

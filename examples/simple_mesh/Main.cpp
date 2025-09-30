4c5381cc43adc0d5b9bac282c2991c3a7dbd98fc
Leorevoir <leo.quinzler@epitech.eu>
2025-09-30 17:56:39 +0200

    add: assets file

--- examples/simple_mesh/Main.cpp
+++ examples/simple_mesh/Main.cpp
@@ -1,5 +1,4 @@
 #include "R-Engine/Application.hpp"
-#include "R-Engine/Core/Filepath.hpp"
 #include "R-Engine/Plugins/DefaultPlugins.hpp"
 #include "R-Engine/Plugins/MeshPlugin.hpp"
 #include "R-Engine/Plugins/WindowPlugin.hpp"
@@ -8,11 +7,11 @@
 
 static void startup_system(r::ecs::Commands &cmd, r::ecs::ResMut<r::Meshes> meshes)
 {
-    const std::string &texture_path = r::path::get("./assets/textures/rock.png");
+    // const std::string &texture_path = r::path::get("./assets/textures/rock.png");
 
     cmd.spawn(
         r::Mesh3d{
-            meshes.ptr->add(r::Mesh3d::Cube(1.f), texture_path),
+            meshes.ptr->add(r::Mesh3d::Cube(1.f)),
             r::Color{ 0, 128, 0}
         },
         r::Transform3d{.translation = {0, 0, 0}, .scale = {1, 1, 1}}

71794eb9679a6499dc3c9fb3033446d1c6535656
Leorevoir <leo.quinzler@epitech.eu>
2025-10-16 14:19:13 +0200

    feat: modular CMakeLists.txt architecture with lualib compilation

--- cmake/3_External.cmake
+++ cmake/3_External.cmake
@@ -1,14 +1,23 @@
+########################################
+# RayLib
+########################################
+
 if (APPLE)
     set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE INTERNAL "Suppress dev warnings")
 endif ()
 
+########################################
+
 add_subdirectory(external/raylib EXCLUDE_FROM_ALL)
+
+########################################
 
 if (APPLE)
     set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 0 CACHE INTERNAL "Re-enable dev warnings")
 endif()
 
-# Removing raylib's warning
+########################################
+
 if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
     target_compile_options(raylib PRIVATE -Wno-stringop-overflow -Wno-undef)
     target_link_options(raylib PRIVATE -Wno-stringop-overflow -Wno-undef)
@@ -16,3 +25,9 @@ elseif (NOT MSVC)
     target_compile_options(raylib PRIVATE -Wno-undef)
     target_link_options(raylib PRIVATE -Wno-undef)
 endif ()
+
+########################################
+# Lua
+########################################
+
+add_subdirectory(external/lua EXCLUDE_FROM_ALL)

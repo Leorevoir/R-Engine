777d45a01e6d0527d371dc31103a4fb328c1a023
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-11-02 21:02:37 +0100

    chore: Remove useless OpenSSL dependency

--- cmake/7_Target.cmake
+++ cmake/7_Target.cmake
@@ -6,12 +6,9 @@ target_include_directories(r-engine PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     ${CMAKE_CURRENT_SOURCE_DIR}/external
 )
-find_package(OpenSSL REQUIRED)
 target_link_libraries(r-engine PUBLIC
     raylib
     lua_lib
-    OpenSSL::SSL
-    OpenSSL::Crypto
     r-type_network
 )
 

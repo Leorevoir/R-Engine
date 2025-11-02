183d218bf920a306e972a2bdd6830659f3f15860
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-11-02 20:43:36 +0100

    fix: Make procedural mesh creation thread-safe

--- src/Plugins/Mesh/Mesh3d.cpp
+++ src/Plugins/Mesh/Mesh3d.cpp
@@ -4,19 +4,76 @@
 #include <R-Engine/Maths/Quaternion.hpp>
 #include <R-Engine/Plugins/MeshPlugin.hpp>
 
+#include <cstring>
+
 /**
 * public
 */
 
 ::Mesh r::Mesh3d::Cube(const f32 size, const Vec3f &center) noexcept
 {
-    const ::Mesh mesh = GenMeshCube(size, size, size);
+    ::Mesh mesh{};
+    mesh.vertexCount = 24;
+    mesh.triangleCount = 12;
 
-    for (i32 i = 0; i < mesh.vertexCount; ++i) {
-        mesh.vertices[i * 3 + 0] += center.x;
-        mesh.vertices[i * 3 + 1] += center.y;
-        mesh.vertices[i * 3 + 2] += center.z;
+    mesh.vertices = (float *) MemAlloc(static_cast<unsigned int>(static_cast<size_t>(mesh.vertexCount) * 3 * sizeof(float)));
+    mesh.texcoords = (float *) MemAlloc(static_cast<unsigned int>(static_cast<size_t>(mesh.vertexCount) * 2 * sizeof(float)));
+    mesh.normals = (float *) MemAlloc(static_cast<unsigned int>(static_cast<size_t>(mesh.vertexCount) * 3 * sizeof(float)));
+    mesh.indices =
+        (unsigned short *) MemAlloc(static_cast<unsigned int>(static_cast<size_t>(mesh.triangleCount) * 3 * sizeof(unsigned short)));
+
+    float half_size = size / 2.0f;
+
+    float p_vertices[] = {/* Front face */
+        -half_size, -half_size, half_size, half_size, -half_size, half_size, half_size, half_size, half_size, -half_size, half_size,
+        half_size,
+        /* Back face */
+        -half_size, -half_size, -half_size, -half_size, half_size, -half_size, half_size, half_size, -half_size, half_size, -half_size,
+        -half_size,
+        /* Top face */
+        -half_size, half_size, -half_size, -half_size, half_size, half_size, half_size, half_size, half_size, half_size, half_size,
+        -half_size,
+        /* Bottom face */
+        -half_size, -half_size, -half_size, half_size, -half_size, -half_size, half_size, -half_size, half_size, -half_size, -half_size,
+        half_size,
+        /* Right face */
+        half_size, -half_size, -half_size, half_size, half_size, -half_size, half_size, half_size, half_size, half_size, -half_size,
+        half_size,
+        /* Left face */
+        -half_size, -half_size, -half_size, -half_size, -half_size, half_size, -half_size, half_size, half_size, -half_size, half_size,
+        -half_size};
+
+    float p_normals[] = {
+        0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,    ///< Front
+        0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1,///< Back
+        0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,    ///< Top
+        0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,//< Bottom
+        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,    ///< Right
+        -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0 ///< Left
+    };
+
+    float p_texcoords[] = {
+        0, 0, 1, 0, 1, 1, 0, 1,///< Front
+        1, 0, 1, 1, 0, 1, 0, 0,///< Back
+        0, 1, 0, 0, 1, 0, 1, 1,///< Top
+        1, 1, 0, 1, 0, 0, 1, 0,///< Bottom
+        1, 0, 1, 1, 0, 1, 0, 0,///< Right
+        0, 0, 1, 0, 1, 1, 0, 1 ///< Left
+    };
+
+    unsigned short p_indices[] = {0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19,
+        20, 21, 22, 20, 22, 23};
+
+    for (int i = 0; i < mesh.vertexCount; i++) {
+        mesh.vertices[i * 3 + 0] = p_vertices[i * 3 + 0] + center.x;
+        mesh.vertices[i * 3 + 1] = p_vertices[i * 3 + 1] + center.y;
+        mesh.vertices[i * 3 + 2] = p_vertices[i * 3 + 2] + center.z;
     }
+
+    memcpy(mesh.normals, p_normals, static_cast<size_t>(mesh.vertexCount) * 3 * sizeof(float));
+    memcpy(mesh.texcoords, p_texcoords, static_cast<size_t>(mesh.vertexCount) * 2 * sizeof(float));
+    memcpy(mesh.indices, p_indices, static_cast<size_t>(mesh.triangleCount) * 3 * sizeof(unsigned short));
+
     return mesh;
 }
 
@@ -28,10 +85,10 @@
     mesh.triangleCount = static_cast<i32>(slices);
     mesh.vertexCount = static_cast<i32>(vertex_count);
 
-    mesh.vertices = (f32 *) MemAlloc(vertex_count * 3 * sizeof(f32));
-    mesh.normals = (f32 *) MemAlloc(vertex_count * 3 * sizeof(f32));
-    mesh.texcoords = (f32 *) MemAlloc(vertex_count * 2 * sizeof(f32));
-    mesh.indices = (unsigned short *) MemAlloc(slices * 3 * sizeof(unsigned short));
+    mesh.vertices = (f32 *) MemAlloc(static_cast<unsigned int>(vertex_count * 3 * sizeof(f32)));
+    mesh.normals = (f32 *) MemAlloc(static_cast<unsigned int>(vertex_count * 3 * sizeof(f32)));
+    mesh.texcoords = (f32 *) MemAlloc(static_cast<unsigned int>(vertex_count * 2 * sizeof(f32)));
+    mesh.indices = (unsigned short *) MemAlloc(static_cast<unsigned int>(slices * 3 * sizeof(unsigned short)));
 
     mesh.vertices[0] = center.x;
     mesh.vertices[1] = center.y;
